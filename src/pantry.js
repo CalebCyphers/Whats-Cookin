7cb3a4e2fa32c07beae4b4573e949bd49ce08f73
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-09-05 17:09:49 -0600

    Add determineIfUserHasEnoughIngredients() method

--- src/pantry.js
+++ src/pantry.js
@@ -2,6 +2,15 @@ class Pantry {
   constructor(userIngredients) {
     this.contents = userIngredients;
   }
-}
 
+  determineIfUserHasEnoughIngredients(recipe) {
+    let pantryIngredientIds = this.contents.map(item => item.ingredient)
+    for (var i = 0; i < recipe.ingredients.length; i++) {
+      if (!pantryIngredientIds.includes(recipe.ingredients[i].id)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
 export default Pantry;
