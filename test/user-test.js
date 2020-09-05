9b2673db0abfe88e4c69fe9e732bf14fd16adc3e
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-09-05 10:57:40 -0600

    Create passing tests for checkPantry() functionality

--- test/user-test.js
+++ test/user-test.js
@@ -29,37 +29,69 @@ describe('User', () => {
 
   it('Should be able to add recipes to favoriteRecipes', () =>{
     user1.addToFavorites(recipeData[0])
+    //add more thorough testing here
     expect(user1.favoriteRecipes.includes(recipeData[0])).to.eql(true);
   });
 
   it('Should be able to remove recipes from favoriteRecipes', () =>{
     user1.removeFromFavorites(recipeData);
+    //add more thorough testing here
     expect(user1.favoriteRecipes).to.eql([]);
   });
 
   it('Should be able to filter through favoriteRecipes by tag', () => {
     user1.addToFavorites(recipeData[0]);
     user1.addToFavorites(recipeData[1]);
+    //search by id property of each recipe?
     expect(user1.filterFavorites('antipasti')).to.eql([recipeData[0]]);
   });
 
   it('Should be able to search favoriteRecipes by name or ingredient', () => {
     user1.addToFavorites(recipeData[0]);
     user1.addToFavorites(recipeData[1]);
+    //add another expect statement for searching by recipe name
     expect(user1.findFavorites('egg')).to.eql([recipeData[0]]);
   });
 
   it('Should be able to check ingredients in User/s pantry for a given recipe', () => {
-    expect(user1.checkPantry(recipeIngredients)).to.eql('You have the ingredients!');
+    const recipe = {
+      name: 'Best Breakfast Ever',
+      ingredients: [
+        {
+          'ingredient': 1077,
+          'amount': 1
+        },
+        {
+          'ingredient': 14412,
+          'amount': 1
+        },
+        {
+          'ingredient': 1009054,
+          'amount': 3
+        }
+      ]};
+    expect(user1.checkPantry(recipe)).to.eql('You have all the ingredients in your pantry!');
   });
 
   it('Should inform User if they lack required ingredients for a given recipe', () => {
-    expect(user1.checkPantry(recipeIngredients)).to.eql(missingIngredientsWithPrice);
+    const recipe = recipeData[0];
+    const pantryIngredients = user1.pantry.map(pantryItem => {
+      return pantryItem.ingredient;
+    })
+    const missingIngredients = recipe.ingredients.filter(item => {
+      return !(pantryIngredients.includes(item.id));
+    })
+    expect(user1.checkPantry(recipe)).to.eql(missingIngredients);
   });
 
-  it('Should be able to add a recipe to recipesToCook', () => {
-    user1.addtoRecipesToCook(recipeData[1]);
+  it('Should start the User off with zero recipes to cook', () => {
+    expect(user1.recipesToCook).to.deep.eql([]);
+    expect(user1.recipesToCook.length).to.eql(0);
+  })
 
-    expect(user1.recipesToCook).to.deep.eql([recipeData[1]]);
+  it('Should be able to add recipes to recipesToCook', () => {
+    user1.addToRecipesToCook(recipeData[1]);
+    user1.addToRecipesToCook(recipeData[3]);
+    expect(user1.recipesToCook).to.deep.eql([recipeData[1], recipeData[3]]);
   });
 });
