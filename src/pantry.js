745aed6b2e3297f18630445e7f23dc6427465518
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-09-10 17:25:09 -0600

    Add and refactor methods to Pantry class

--- src/pantry.js
+++ src/pantry.js
@@ -1,103 +1,46 @@
 class Pantry {
   constructor(userIngredients) {
     this.contents = userIngredients;
-    this.goodAmount;
-    this.notEnough = []
   }
-
   getIdsOfPantryIngredients() {
     return this.contents.map(ing => {
-      return ing.ingredient 
+      return ing.ingredient
     })
   }
-
   findRecipeIngredientsInPantry(recipe) {
     let currentPantryIds = this.getIdsOfPantryIngredients()
-    console.log(recipe.ingredients)
     return recipe.ingredients.filter(ingredient => {
       return currentPantryIds.includes(ingredient.id)
     })
   }
-
   doesUserHaveRequiredIngredients(recipe) {
-    console.log(recipe.ingredients)
-    return this.findRecipeIngredientsInPantry().length === recipe.ingredients.length
+    return this.findRecipeIngredientsInPantry(recipe).length === recipe.ingredients.length
   }
-
-  findWhichIngredientsAreShort() { 
-    let notEnough = this.findRecipeIngredientsInPantry().filter(ing => {
-      let ingredientStock = this.contents[this.getIdsOfPantryIngredients().indexOf(ing.id)].amount
+  findWhichIngredientsAreShort(recipe) {
+    let notEnough = this.findRecipeIngredientsInPantry(recipe).filter(ing => {
+      let ingredientStock = this.findIngredientInPantry(ing).amount
       return ingredientStock < ing.quantity.amount
     })
     return notEnough
   }
-
   findAmountMissing(recipe) {
-//invoke determineIfUser... ()recipe
-    //if returns false => 
-    //find which ingredients are not satisfied by pantry
-  if (!this.determineIfUserHasEnoughIngredients(recipe)) { 
-    return this.notEnough.length
-    }
+    let missingIngredients = this.findWhichIngredientsAreShort(recipe)
+   return missingIngredients.reduce((finalArray, ingredient) => {
+      finalArray.push({ name: ingredient.name, facts: { id: ingredient.id, amountMissing: ingredient.quantity.amount - this.findIngredientInPantry(ingredient).amount}})
+      return finalArray
+    },[])
+  }
+  calculateMissingIngredientCost(recipe) {
+    let ingredientsMissing = this.findAmountMissing(recipe)
+     return  ingredientsMissing.reduce((string, ingredient )=>{
+      string += ` ${ingredient.facts.amountMissing} ${ingredient.name}\n`
+      return string
+    },"You are missing")
+  }
+  findIngredientInPantry(ingredient) {
+    return this.contents.find(pantryIngredient => {
+      return pantryIngredient.ingredient === ingredient.id
+    })
   }
 }
-
 export default Pantry;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let pantryIngredientIds = this.contents.map(item => item)
-// for (var i = 0; i < 10; i++) {
-//   for (let j = 0 ; j < 10; j++) {
-
-//   }
-//   console.log(`${i},${j}`)
-// }
-//   if (!pantryIngredientIds.includes(recipe.ingredients[i].id)) {
-//     return false;
-//   }
-// }
-// return true;
-// })
-
-// input - an array of required ingredients and a required quantity.amount
-// input - an array of ingredients that are stocked in the pantry (this.contents)
-// output - a boolean reflecting wether or not the pantry contents have the required ingredients
-
-// let pantryIngredientIds = this.contents.map(item => item)
-// recipe.ingredients.filter((ingredient, i) => {
-//   pantryIngredientIds.forEach() {
-
-//   }
-//   if (pantryIngredientIds.ingredient === ingredient.id) {
-//     return ingredient.quantity.amount >= pantryIngredientIds[i].amount
-//   }
-
-    // let reMappedRecipeIngredients = recipe.ingredients.map(recipeIngredient => {
-//   console.log(recipeIngredient);
-//   return {
-//     ingredient: recipeIngredient.id,
-//     amount: recipeIngredient.quantity.amount
-//   }
-// })
-
-// reMappedRecipeIngredients.forEach(item => {
-//   this.contents.includes
-// })
-
