c2b7842bb2ac0391b0cd9178766b5ab2a8ecd64e
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-09-11 17:36:11 -0600

    Add grabUsers() to fetch User data

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,15 +1,29 @@
 let domUpdates = {
-
-    grabRecipes() {
+  
+  grabRecipes() {
     fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/recipes/recipeData')
     .then(response => response.json())
     .then(recipeObject => {
-     recipeObject.recipeData.forEach(recipe =>{
+      recipeObject.recipeData.forEach(recipe =>{
        displayAllRecipes(recipe);
-     })
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      alert('Sorry, we could not load the recipes at this time.')})
+  },
+
+  grabUsers() {
+    fetch('https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/users/wcUsersData',)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+        let currentUser = data.wcUsersData.find(user => user.id === (Math.floor(Math.random() * 49) + 1))
     })
     .catch(err => {
         console.log(err);
-        alert('Sorry, we could not load the recipes at this time.')})
+        alert('Sorry, the user information failed to load. Try again later.');
+    })
   }
 };
